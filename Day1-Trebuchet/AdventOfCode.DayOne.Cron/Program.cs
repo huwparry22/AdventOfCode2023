7a18769113068f1f3da8c10625ddad3dac64c484
Huw Parry <huparry@deloitte.co.uk>
2023-12-01 13:59:05 +0000

    Added solution.

--- Day1-Trebuchet/AdventOfCode.DayOne.Cron/Program.cs
+++ Day1-Trebuchet/AdventOfCode.DayOne.Cron/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllLines("input.txt");
+
+List<int> values = new List<int>();
+
+foreach(var item in input)
+{
+    var first = item.First(c => char.IsDigit(c));
+    var last = item.Last(c => char.IsDigit(c));
+
+    char[] arrNumber = {first, last};
+
+    values.Add(Convert.ToInt32(new string(arrNumber)));
+}
+
+Console.WriteLine(values.Sum());
+
