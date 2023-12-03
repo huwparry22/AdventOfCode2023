1067fa4e8a16b26a2744d5267d3483f3ab483e78
Huw Parry <huwparry22@hotmail.com>
2023-12-03 12:26:05 +0000

    Started adding part one implementation.

---
+++ Day2-CubeConundrum/AdventOfCode.DayTwo.Cron/PartOne.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode.DayTwo.Cron;
+
+public class PartOne
+{
+    public void Run(IEnumerable<string> input)
+    {
+
+    }
+
+    /// <summary>
+    /// E.g Game Input - "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+    /// </summary>
+    /// <param name="gameInput"></param>
+    /// <returns></returns>
+    public bool IsGamePossible(string gameInput)
+    {
+        var splitGame = gameInput.Split(":");
+
+        var cubeSets = splitGame[1].Split(";");
+        foreach(var cubeSet in cubeSets)
+        {
+            var cubes = cubeSet.Split(",");
+            
+        }
+
+
+
+        return false;
+    }
+}
